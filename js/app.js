e1c9b8fb64ec8ff51ecdf99ef812a0bb9a9fc92b
Noah Schade <noah9@mail.com>
2019-03-28 19:43:32 -0400

    Update 'js/app.js' file

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
-// const game = new Game();
-// game.createPhrases('phrase1 aSDSsdf', 'phrase2 asasdfdf', 'pasdfhrase3 asdf', 'phrasdfase4 asdf', 'phrasadfe5 asdf');
-// game.startGame();
-// console.log(`Active Phrase - phrase: ${game.activePhrase}`);
+const game = new Game();
+game.createPhrases('phrase1 aSDSsdf', 'phrase2 asasdfdf', 'pasdfhrase3 asdf', 'phrasdfase4 asdf', 'phrasadfe5 asdf');
 
-let game;
+const phrase = new Phrase;
+phrase.addPhraseToDisplay()
+console.log(phrase.phrase);
+
+
 document.getElementById("btn__reset").addEventListener("click", function(){
-    game = new Game;
     game.startGame();
+});
+
+
+
+document.getElementById("qwerty").addEventListener("click", function(){
+    let key = event.target.textContent;
+    for(let i = 0; i < phrase.phrase.length; i++) {
+        if (key === phrase.phrase[i]) {
+            console.log("match");
+        }
+    }
 });
