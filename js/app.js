27beeb7517d86725f92ffac2faf1e49be9e14c7d
Noah Schade <noah9@mail.com>
2019-03-31 20:39:42 -0400

    Add mousedown to event listeners

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const game = new Game;
 
 
-document.getElementById("btn__reset").addEventListener("click", function(){
+document.getElementById("btn__reset").addEventListener("mousedown", function(){
     game.startGame();
 });
 
 
 
-document.getElementById("qwerty").addEventListener("click", function() {
+document.getElementById("qwerty").addEventListener("mousedown", function() {
     for(let i = 0; i < document.querySelectorAll(".section .keyrow .key").length; i++) {
         if(event.target === document.querySelectorAll(".section .keyrow .key")[i]) {
             newPhrase.checkLetter(game.activePhrase.toLowerCase());
